c6a18d27535991ea0ac7e2266d8c6e7004826371
Valerria Hutsuliak <valeria.gutsuliak@gmail.com>
2019-10-03 17:31:34 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,23 @@
 # Add your variables here
+first_number = 8
+second_number = 5
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
+
+def sum(first_number, second_number) 
+    first_number + second_number
+end 
+
+def difference(first_number, second_number)
+    difference = first_number - second_number
+end 
+
+def product(first_number, second_number)
+    product = first_number * second_number
+end 
+
+def quotient(first_number, second_number)
+    quotient = first_number / second_number
+end  
